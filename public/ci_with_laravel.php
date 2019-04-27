e1159d65f71f0f973cb025ac6216b562fbf1d551
kkame <kkamenet@gmail.com>
2019-04-27 18:24:59 +0900

    ci에서 laravel 정보 가져오기 위한 설정 추가

--- public/ci_with_laravel.php
+++ public/ci_with_laravel.php
@@ -6,5 +6,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 ## load laravel
 require_once __DIR__ . '/../bootstrap/app.php';
 
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
 ## load codeigniter
 require_once __DIR__ . '/ci.php';
