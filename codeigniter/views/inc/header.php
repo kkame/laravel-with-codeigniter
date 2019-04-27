f29718ae3267cc89bec3897214cacb8d7b2ef846
kkame <kkamenet@gmail.com>
2019-04-27 18:26:51 +0900

    tagmanager 추가

--- codeigniter/views/inc/header.php
+++ codeigniter/views/inc/header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php if(config('tagmanager.id')){?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','<?=config('tagmanager.id')?>');</script>
+    <!-- End Google Tag Manager -->
+    <?php }?>
     <title>WebUni - Education Template</title>
     <meta charset="UTF-8">
     <meta name="description" content="WebUni Education Template">
---
+++ config/tagmanager.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'id' => env('TAG_MANAGER_ID'),
+];
